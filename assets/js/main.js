24c71329aef44ca7aa42bf9d06b029f2a28c81c8
DucPhan <phantaiduc2005@gmail.com>
2022-07-30 13:35:45 +0700

    Chore: delete gigachad imgs & split main.js

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+const deg = 6;
+const hr = document.querySelector('#hr');
+const mn = document.querySelector('#mn');
+const sc = document.querySelector('#sc');
+
+const hr_gl = document.querySelector('#hr_gl');
+const mn_gl = document.querySelector('#mn_gl');
+const sc_gl = document.querySelector('#sc_gl');
+
+
+setInterval(
+    () => {
+
+        let day = new Date();
+        let hh = day.getHours() * 30;
+        let mm = day.getMinutes() * deg;
+        let ss = day.getSeconds() * deg;
+
+        hr.style.transform = `rotateZ(${(hh) + (mm / 12)}deg)`;
+        mn.style.transform = `rotateZ(${mm}deg)`;
+        sc.style.transform = `rotateZ(${ss}deg)`;
+
+        hr_gl.style.transform = `rotateZ(${(hh) + (mm / 12)}deg)`;
+        mn_gl.style.transform = `rotateZ(${mm}deg)`;
+        sc_gl.style.transform = `rotateZ(${ss}deg)`;
+    }
+)
+
+
+function popupToggle() {
+    const popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
